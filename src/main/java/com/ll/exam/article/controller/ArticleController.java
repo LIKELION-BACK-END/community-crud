24663eb81f83480f71684a61188a3d1567c1489c
zzerowin <alaykr123@gmail.com>
2022-08-04 15:16:23 +0900

    상세페이지 구현, 리스트페이지에 링크

--- src/main/java/com/ll/exam/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/article/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.ll.exam.Rq;
 import com.ll.exam.annotation.Autowired;
 import com.ll.exam.annotation.Controller;
 import com.ll.exam.annotation.GetMapping;
+import com.ll.exam.annotation.PostMapping;
 import com.ll.exam.article.dto.ArticleDto;
 import com.ll.exam.article.service.ArticleService;
 
@@ -22,5 +23,50 @@ public class ArticleController {
 
         rq.setAttr("articles", articleDtos);
         rq.view("usr/article/list");
+    }
+
+    @GetMapping("/usr/article/write")
+    public void showWrite(Rq rq) {
+        rq.view("usr/article/write");
+    }
+
+    @PostMapping("/usr/article/write")
+    public void write(Rq rq) {
+        String title = rq.getParam("title", "");
+        String body = rq.getParam("body", "");
+
+        if (title.length() == 0) {
+            rq.historyBack("제목을 입력해주세요.");
+            return;
+        }
+
+        if (body.length() == 0) {
+            rq.historyBack("내용을 입력해주세요.");
+            return;
+        }
+
+        long id = articleService.write(title, body);
+
+        rq.replace("/usr/article/detail/free/%d".formatted(id), "%d번 게시물이 생성 되었습니다.".formatted(id));
+    }
+
+    @GetMapping("/usr/article/detail/{id}")
+    public void showDetail(Rq rq) {
+        long id = rq.getLongParam("id", 0);
+
+        if (id == 0) {
+            rq.historyBack("번호를 입력해주세요.");
+            return;
+        }
+
+        ArticleDto articleDto = articleService.getArticleById(id);
+
+        if (articleDto == null) {
+            rq.historyBack("해당 글이 존재하지 않습니다.");
+            return;
+        }
+
+        rq.setAttr("article", articleDto);
+        rq.view("usr/article/detail");
     }
 }
