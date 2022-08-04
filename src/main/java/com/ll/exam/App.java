1b55252cf7e36686c37e2424e7cadbd1f12d7364
zzerowin <alaykr123@gmail.com>
2022-08-04 10:30:25 +0900

    프로젝트 생성

--- src/main/java/com/ll/exam/App.java
+++ src/main/java/com/ll/exam/App.java
@@ -1,6 +1,7 @@
 package com.ll.exam;
 
 public class App {
+
     public static final String BASE_PACKAGE_PATH = "com.ll.exam";
     public static final String DB_HOST = "localhost";
     public static final int DB_PORT = 3306;
